7294a6fba4df77792c906427418c6c51264281c3
Tanel Laud <tanel@arego.ee>
2016-10-03 19:42:47 +0300

    first commit on movietickets

---
+++ src/movietickets.java
@@ -0,0 +1,8 @@
+/*
+ * Created by Tanel Laud on 03.10.2016.
+ */
+public class movietickets {
+    public static void main(String[] args) {
+        System.out.println("Hello movie tickets ");
+    }
+}
